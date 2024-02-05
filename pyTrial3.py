ec43100eceb6f996a62a6a542023f51035f1dec1
SaraSalvador1991 <sarasalva91@gmail.com>
2024-02-05 14:14:01 +0100

    Create pyTrial3.py

---
+++ pyTrial3.py
@@ -0,0 +1,11 @@
+# store input numbers
+x=input('Enter the first number')
+y=input('Enter the second number')
+
+# add the two numbers
+
+sum = float(x) + float (y)
+
+# display the two numbers
+
+print('The sum of {0} and {1} is {2}'.format(x,y,sum))
