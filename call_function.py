360f098f3f2a6eaf621a4a519820f8b9cee6b10c
SaraSalvador1991 <sarasalva91@gmail.com>
2024-02-05 15:50:33 +0100

    new function and call

--- call_function.py
+++ call_function.py
@@ -1,3 +1,4 @@
 from fun_Prime import fun_Prime
-
+from fun_sign import sgn
 fun_Prime(11)
+sgn(-9)
---
+++ fun_sign.py
@@ -0,0 +1,9 @@
+def sgn(x):
+    if x>0:
+        print('{0} is a positive number'.format(x))
+    elif x==0:
+        print('{0} is the number zero'.format(x))
+    else:
+        print('{0} is a negative number'.format(x))
+
+
